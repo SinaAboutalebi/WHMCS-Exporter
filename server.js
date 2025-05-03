e230630bcd7172d1ec284d518b38b0776f15b250
SinaAboutalebi <zeroopoower@gmail.com>
2025-05-03 08:56:21 +0330

    feat: Update console log to include full URL for WHMCS Prometheus exporter

--- server.js
+++ server.js
@@ -12,5 +12,5 @@ app.get('/metrics', async (req, res) => {
 });
 
 app.listen(PORT, () => {
-  console.log(`WHMCS Prometheus exporter listening on port ${PORT}`);
+  console.log(`[🟢] WHMCS Prometheus exporter listening on http://localhost:${PORT}`);
 });
